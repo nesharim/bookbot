436dee8372c53209b845c9322b10483adc5619e3
Nesharim <raphaelkribeiro@gmail.com>
2025-06-20 00:30:15 -0400

    adds cli params to read a book in a file path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import count_words
 from stats import count_character_occurences
 from stats import sorted_chars_list
+import sys
 
 def get_book_text(filepath):
     file_contents = None
@@ -10,12 +11,18 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    file_contents = get_book_text("./books/frankenstein.txt")
+    args = sys.argv
+    if len(args) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file_path = args[1]
+    file_contents = get_book_text(file_path)
     num_words = count_words(file_contents)
     char_occurrences = count_character_occurences(file_contents)
     sorted_char_count = sorted_chars_list(char_occurrences)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
